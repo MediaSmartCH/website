415d801444feb80fc3f630bd619f84bf7e73a907
rphlr <81936759+rphlr@users.noreply.github.com>
2024-03-10 14:15:49 +0100

    feat(analytics): add Vercel Analytics and Speed Insights to the homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -11,6 +11,8 @@ import Services from "components/presentation/Services";
 import Booking from "components/presentation/Booking";
 import Testimonials from "components/presentation/Testimonials";
 import Contact from "components/presentation/Contact";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const Homepage = () => {
   React.useEffect(() => {
@@ -22,6 +24,8 @@ const Homepage = () => {
   }, []);
   return (
     <div>
+      <Analytics />
+      <SpeedInsights />
       {/* <div className="hero-bg"> */}
       <Navbar />
       <Hero />
