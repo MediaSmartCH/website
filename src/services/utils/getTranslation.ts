785b2a72d4371bfb441534d8853311158c615a45
rphlr <81936759+rphlr@users.noreply.github.com>
2024-03-20 06:49:06 +0100

    refactor(ui): remove AOS-based animations from the landing page

--- src/services/utils/getTranslation.ts
+++
@@ -1,9 +0,0 @@
-import { dictionary } from "services/resources/multiLanguages";
-
-export const getTranslation = (
-  section: string,
-  key: string,
-  currentLanguage: string
-) => {
-  return dictionary[section][currentLanguage][key];
-};
